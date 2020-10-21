01f270bb51c43bf1e144c1026d217f9210aecf3b
Mohd <alfonso.salik@gmail.com>
2020-10-22 02:28:24 +0800

    v0.0.1 update

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyAI : MonoBehaviour
+{
+    int counter = 500;
+    private bool FacingRight = true;
+    
+    void move(){
+        if (counter == 0){
+            counter = 500;
+            Flip();
+        }else{
+            counter --;
+        }
+    }
+
+    void Update(){   
+        move();
+    }
+
+    //call this function to flip
+    private void Flip(){
+        // Debug.Log("FLIPPED");
+		FacingRight = !FacingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
